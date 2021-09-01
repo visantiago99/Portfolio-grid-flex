965ec1949c5b2978df847a20de500811829917b5
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-09-01 19:26:37 -0300

    Add hamburguer menu for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import { SiLinkedin, SiGithub } from "react-icons/si";
 
 export default function Header() {
+  const[activeState, setActive] = useState(false);
+
+
   return (
     <div className="header">
-      <div className="logo"></div>
-      <ul className="menu">
-        <Link to="/"><li><button className="btn-header">HOME</button></li></Link>
-        <Link to="/projects"><li><button className="btn-header">PROJECTS</button></li></Link>
-        <Link to="/contact"><li><button className="btn-header">CONTACT</button></li></Link>
-      </ul>
+      <div className="navbar">
+        <div className="logo"></div>
+        <div className="hamburguer active">
+          <button type="button" onClick={() => setActive(!activeState)}></button>
+        </div>
+        <ul className={activeState ? "menu active" : "menu"}>
+          <Link to="/"><li><button className="btn-header">HOME</button></li></Link>
+          <Link to="/projects"><li><button className="btn-header">PROJECTS</button></li></Link>
+          <Link to="/contact"><li><button className="btn-header">CONTACT</button></li></Link>
+        </ul>
+      </div>
       <div>
         <a href='https://www.linkedin.com/in/victor-santiago-082542144/' target='_blank' rel='noreferrer'><SiLinkedin /></a>
         <a href='https://github.com/visantiago99' target='_blank' rel='noreferrer'><SiGithub /></a>
