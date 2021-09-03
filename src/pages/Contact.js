9236db67b7b1feb7b0a591bf04c85562ae930216
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-09-03 14:20:46 -0300

    Update contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 import ContactUs from '../components/ContactUs';
 import { SiLinkedin, SiGithub } from "react-icons/si";
 
@@ -8,13 +9,16 @@ export default function Contact() {
   return (
     <div>
       <Header />
-      <h1>You can reach me here:</h1>
-      <br />
-      <div className="divSocialContact">
+      <div className="contact-container">
+        <h1>You can send me a email:</h1>
+        <ContactUs />
+        <h1>You can also reach me here:</h1>
+        <div className="divSocialContact">
           <a href='https://www.linkedin.com/in/victor-santiago-082542144/' target='_blank' rel='noreferrer'><SiLinkedin /></a>
           <a href='https://github.com/visantiago99' target='_blank' rel='noreferrer'><SiGithub /></a>
         </div>
-      <ContactUs />
+      </div>
+      <Footer />
     </div>
   )
 }
