2945d3459bf5305994cc64e9984859042251fd65
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-09-03 00:18:36 -0300

    Projects page update

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,53 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import recipe from '../images/recipe.gif';
+import trivia from '../images/trivia.gif';
+import money from '../images/money.gif';
+import sql from '../images/sql.gif';
+
 
 export default function Projects() {
   return (
     <div>
       <Header />
+      <div className="main-container-proj">
+          <div className="p-item">
+              <a href='https://github.com/visantiago99/Recipes-App' target='_blank' rel='noreferrer'><img src={recipe} alt="recipe-gif" /></a>
+              {/*365x454*/}
+          </div>
+          <div className="i-item">
+            <h2>info recipe</h2>
+          </div>
+          <div className="p-item">
+              <a href='https://github.com/visantiago99/Trivia-App-React-Redux' target='_blank' rel='noreferrer'><img src={trivia} alt="trivia-gif" /></a>
+          </div>
+          <div className="i-item">
+            <h2>info Trivia</h2>
+          </div>
+          <div className="p-item">
+              <a href='https://github.com/visantiago99/Wallet-Coin-Converter' target='_blank' rel='noreferrer'><img src={money} alt="money-gif" /></a>
+          </div>
+          <div className="i-item">
+            <h2>info wallet</h2>
+          </div>
+          <div className="p-item">
+              <a href='https://github.com/visantiago99/MYSQL-vocabulary-booster' target='_blank' rel='noreferrer'><img src={sql} alt="sql-gif"/></a>
+          </div>
+          <div className="i-item">
+            <h2>info mysql</h2>
+          </div>
+      </div>
+        <div className="git-container">
+          <div>
+            <h2>You can check other projects and all of my repositories by clicking in the Octo Cat</h2>
+          </div>
+          <div>
+          <a href='https://github.com/visantiago99' target='_blank' rel='noreferrer'>
+            <img src="https://octocat-generator-assets.githubusercontent.com/my-octocat-1628983230728.png" alt="myOcto" width="350px" height="350px" className="octo"/>
+          </a>
+          </div>
+        </div>
       <Footer />
     </div>
   )
