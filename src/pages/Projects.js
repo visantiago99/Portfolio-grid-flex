a8fc4fc85fbe2a029329e198d0d5b66be5e345cc
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-09-09 16:57:19 -0300

    Projects page update

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,7 +4,7 @@ import Footer from '../components/Footer';
 import recipe from '../images/recipe.gif';
 import trivia from '../images/trivia.gif';
 import money from '../images/money.gif';
-import sql from '../images/sql.gif';
+import shoppingcart from '../images/shoppingcart.gif';
 
 
 export default function Projects() {
@@ -14,29 +14,28 @@ export default function Projects() {
       <div className="main-container-proj">
           <div className="p-item">
               <a href='https://github.com/visantiago99/Recipes-App' target='_blank' rel='noreferrer'><img src={recipe} alt="recipe-gif" /></a>
-              {/*365x454*/}
           </div>
           <div className="i-item">
-            <h2>A recipe App made with Hooks and context API, <br /> wich you can see, search, filter, favorite and <br /> see all the steps of a food or drink.
+            <h2>A recipe App made with React, Hooks and context API, where you can see, search, filter, favorite and <br /> see all the steps of a food or drink.
             </h2>
           </div>
           <div className="p-item">
               <a href='https://github.com/visantiago99/Trivia-App-React-Redux' target='_blank' rel='noreferrer'><img src={trivia} alt="trivia-gif" /></a>
           </div>
           <div className="i-item">
-            <h2>info Trivia</h2>
+            <h2>An app quiz like, made with React and Redux, where you can make your login to play, then your highest score will be displayed at the rank page of all players</h2>
           </div>
           <div className="p-item">
               <a href='https://github.com/visantiago99/Wallet-Coin-Converter' target='_blank' rel='noreferrer'><img src={money} alt="money-gif" /></a>
           </div>
           <div className="i-item">
-            <h2>info wallet</h2>
+            <h2>An expense management app, made with React and Redux, where you can convert your expenses too many currencies</h2>
           </div>
           <div className="p-item">
-              <a href='https://github.com/visantiago99/MYSQL-vocabulary-booster' target='_blank' rel='noreferrer'><img src={sql} alt="sql-gif"/></a>
+              <a href='https://github.com/visantiago99/shopping-cart' target='_blank' rel='noreferrer'><img src={shoppingcart} alt="shopping-gif"/></a>
           </div>
           <div className="i-item">
-            <h2>info mysql</h2>
+            <h2>An shopping cart page, like Amazon, where you can add and remove products from cart and filter products by categories and by words</h2>
           </div>
       </div>
         <div className="git-container">
