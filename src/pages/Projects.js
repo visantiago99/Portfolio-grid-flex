b403f13430e53d8a12839db83d377ac8e353b300
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-09-14 21:25:00 -0300

    Projects page updated

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -21,24 +21,28 @@ export default function Projects() {
           <div className="i-item">
             <h2>A recipe App made with React, Hooks and context API, where you can see, search, filter, favorite and <br /> see all the steps of a food or drink.
             </h2>
+            <a href="https://recipe-app-madeby-vs99.netlify.app/" target="_blank" rel="noreferrer">https://recipe-app-madeby-vs99.netlify.app/</a>
           </div>
           <div className="p-item">
               <a href='https://github.com/visantiago99/Trivia-App-React-Redux' target='_blank' rel='noreferrer'><img src={trivia} alt="trivia-gif" /></a>
           </div>
           <div className="i-item">
             <h2>An app quiz like, made with React and Redux, where you can make your login to play, then your highest score will be displayed at the rank page of all players</h2>
+            <a href="https://quiz-game-redux.netlify.app/" target="_blank" rel="noreferrer">https://quiz-game-redux.netlify.app/</a>
           </div>
           <div className="p-item">
               <a href='https://github.com/visantiago99/Wallet-Coin-Converter' target='_blank' rel='noreferrer'><img src={money} alt="money-gif" /></a>
           </div>
           <div className="i-item">
             <h2>An expense management app, made with React and Redux, where you can convert your expenses too many currencies</h2>
+            <a href="https://wallet-and-currency-converter.netlify.app/" target="_blank" rel="noreferrer">https://wallet-and-currency-converter.netlify.app/</a>
           </div>
           <div className="p-item">
               <a href='https://github.com/visantiago99/shopping-cart' target='_blank' rel='noreferrer'><img src={shoppingcart} alt="shopping-gif"/></a>
           </div>
           <div className="i-item">
-            <h2>An shopping cart page, like Amazon, where you can add and remove products from cart and filter products by categories and by words</h2>
+            <h2>A shopping cart page, like Amazon, where you can add and remove products from cart and filter products by categories and by words</h2>
+            <a href="https://shopping-cart-mb.netlify.app/" target="_blank" rel="noreferrer">https://shopping-cart-mb.netlify.app/</a>
           </div>
       </div>
         <div className="git-container">
